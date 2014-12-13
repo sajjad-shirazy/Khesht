e54d07e1fd7b1f65c0f276caa2fd8cd42e3b4250
Sajjad Shirazy <shirazy.sajjad@gmail.com>
2014-12-13 09:56:41 +0330

    version v1.1

--- config.js
+++ config.js
@@ -3,14 +3,22 @@
         name: 'Khesht',
         version: '1.1',
         waitSeconds: 120, //make sure it is enough to load all gmaps scripts
+        baseUrl: '//cdn.rawgit.com/sajjad-shirazy/Khesht/v1.1/',//change it to CDN url of your files
         paths: {
-            requirejs: '//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.15/require.min',
+            requirejs: 	'//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.15/require.min',
             stylesheet: '//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.5/css.min',
-            async: '//cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/async.min', //alias to plugin for JSONP and asynchronous dependencies (e.g. Google Maps).
-            jquery: '//code.jquery.com/jquery-2.1.1.min.js',
-            nprogress: '//cdnjs.cloudflare.com/ajax/libs/nprogress/0.1.6',
-            khesht: '//cdn.rawgit.com/sajjad-shirazy/Khesht/v1.1/min/',
-            bootstrap: '//maxcdn.bootstrapcdn.com/bootstrap/3.3.1'
+            async: 		'//cdnjs.cloudflare.com/ajax/libs/requirejs-plugins/1.0.3/async.min', //alias to plugin for JSONP and asynchronous dependencies (e.g. Google Maps).
+            jquery: 	'//code.jquery.com/jquery-2.1.1.min.js',
+            nprogress: 	'//cdnjs.cloudflare.com/ajax/libs/nprogress/0.1.6',
+            bootstrap: 	'//maxcdn.bootstrapcdn.com/bootstrap/3.3.1',
+			fileinput:	'//cdn.rawgit.com/kartik-v/bootstrap-fileinput/tree/v3.0.0',
+			// ---
+            khesht: 	'//cdn.rawgit.com/sajjad-shirazy/Khesht/v1.1/khesht.min',
+            pages: 		'pages.min',
+            components: 'components.min',
+            css: 		'css.min',
+            images: 	'images',
+            api: 		'/api'
         }
     }
 };
